d1aa9a5dff0cae0e217f6bea007e73e156c8e72a
gaelaffo14-design <gaelaffo14@gmail.com>
2026-08-08 12:48:01 +0100

    Update admin-auth.js

--- js/js/admin-auth.js
+++ js/js/admin-auth.js
@@ -1,1 +1,39 @@
+import {
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut
+} from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
 
+import { auth } from "./firebase.js";
+
+/* =========================
+   VÉRIFICATION DE CONNEXION
+========================= */
+
+onAuthStateChanged(auth, (user) => {
+
+  if (!user) {
+    window.location.href = "login.html";
+  }
+
+});
+
+/* =========================
+   DÉCONNEXION
+========================= */
+
+window.logoutAdmin = async function () {
+
+  try {
+
+    await signOut(auth);
+
+    window.location.href = "login.html";
+
+  } catch (error) {
+
+    console.error("Erreur de déconnexion :", error);
+
+  }
+
+};
